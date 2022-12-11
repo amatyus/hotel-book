4d476e8c6a79918711d226caf91808a3e8afe6a8
Anastasiya Matyushkina <nasty@MacBook-Air-Anastasiya.local>
2022-12-12 00:51:49 +0600

    backend patr 3

--- server/routes/booking.routes.js
+++ server/routes/booking.routes.js
@@ -1,4 +1,52 @@
 const express = require('express')
+const auth = require('../middleware/auth.middleware')
+const Booking = require('../models/Booking')
 const router = express.Router({mergeParams: true})
+
+router
+  .route('/')
+  .get(auth, async (req, res) => {
+    try {
+      const {orderBy, equalTo} = req.query
+      const list = await Booking.find({[orderBy]: equalTo})
+      res.send(list)
+    } catch (e) {
+      res.status(500).json({
+        message: 'На сервере произошла ошибка. Попробуйте позже'
+      })
+    }
+  })
+
+  .post(auth, async (req, res) => {
+    try {
+      const newBooking = await Booking.create({
+        ...req.body,
+        userId: req.user._id
+      })
+      res.status(201).send(newBooking)
+    } catch (e) {
+      res.status(500).json({
+        message: 'На сервере произошла ошибка. Попробуйте позже'
+      })
+    }
+  })
+
+// router.delete('/:roomId', auth, async (req, res) => {
+//   try {
+//     const {roomId} = req.params
+//     const removedRoom = await Booking.findById(roomId)
+
+//     if (removedRoom.userId.toString() === req.user._id) {
+//       await removedRoom.remove()
+//       return res.send(null)
+//     } else {
+//       res.status(401).json({message: 'Unauthorized'})
+//     }
+//   } catch (e) {
+//     res.status(500).json({
+//       message: 'На сервере произошла ошибка. Попробуйте позже'
+//     })
+//   }
+// })
 
 module.exports = router
--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -1,4 +1,69 @@
 const express = require('express')
+const Rooms = require('../models/Rooms')
+const auth = require('../middleware/auth.middleware')
+const {generateUserData} = require('../utils/helper')
 const router = express.Router({mergeParams: true})
+
+router.patch('/:roomId', auth, async (req, res) => {
+  try {
+    const {roomId} = req.params
+
+    if (roomId === req.room._id) {
+      const updatedRoom = await Rooms.findByIdAndUpdate(roomId, req.body, {
+        new: true
+      })
+      res.send(updatedRoom)
+    } else {
+      res.status(401).json({message: 'Unauthorized'})
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже'
+    })
+  }
+})
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const list = await Rooms.find()
+    res.send(list)
+  } catch (e) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже'
+    })
+  }
+})
+
+router.post('/', auth, async (req, res) => {
+  try {
+    const newRoom = await Rooms.create({
+      ...generateUserData(),
+      ...req.body
+    })
+    res.status(201).send(newRoom)
+  } catch (e) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже'
+    })
+  }
+})
+
+router.delete('/:roomId', auth, async (req, res) => {
+  try {
+    const {roomId} = req.params
+    const removedRoom = await Booking.findById(roomId)
+
+    if (removedRoom.userId.toString() === req.user._id) {
+      await removedRoom.remove()
+      return res.send(null)
+    } else {
+      res.status(401).json({message: 'Unauthorized'})
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже'
+    })
+  }
+})
 
 module.exports = router
